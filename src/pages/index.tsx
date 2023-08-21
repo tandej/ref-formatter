c9a6596c20c10db0821f6c8794959c01918158f7
Thomas Anderson <tande92834@gmail.com>
2023-08-20 22:13:03 -0700

    Just use CSS instead>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,26 @@
 import React from 'react';
-import {
-  Button,
-  Box,
-  ThemeProvider,
-  CssBaseline,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  SelectChangeEvent,
-} from '@mui/material';
 import theme from '../theme';
 
 function BasicSelect() {
   const [formatType, setFormatType] = React.useState('');
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setFormatType(event.target.value as string);
-  };
-
   return (
-    <Box sx={{minWidth: 120}}>
-      <FormControl fullWidth>
-        <InputLabel id="FormatType">Format Type</InputLabel>
-
-        <Select id="FormatType" value={formatType} onChange={handleChange}>
-          <MenuItem value={0}>MLA</MenuItem>
-          <MenuItem value={1}>bibTeX</MenuItem>
-          <MenuItem value={2}>yaml</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    <html><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+    <label>
+      Select Formatter:
+      <select name="selectedFormatter">
+        <option value="MLA">MLA</option>
+        <option value="BibTeX">BibTex</option>
+        <option value="yaml">yaml</option>
+      </select>
+    </label></html>
   );
 }
 
 const IndexPage = () => {
   const age: number = 0;
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box p={4}>
-        <Button variant="contained">It fucking works!!!</Button>
-        <p />
-        <BasicSelect />
-      </Box>
-    </ThemeProvider>
-  );
+  return (BasicSelect());
 };
 
 export default IndexPage;
