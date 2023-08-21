713ecf6d9210667ddceb472b558ba52644a1aa9f
Thomas Anderson <tande92834@gmail.com>
2023-08-21 04:35:14 -0700

    Move css styling to global scope

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import theme from '../theme';
 
-function BasicSelect() {
+function formatterSelect() {
   const [formatType, setFormatType] = React.useState('');
 
   return (
-    <html><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
-    <label>
-      Select Formatter:
-      <select name="selectedFormatter">
-        <option value="MLA">MLA</option>
-        <option value="BibTeX">BibTex</option>
-        <option value="yaml">yaml</option>
-      </select>
-    </label></html>
+      <label>
+        Select Formatter:
+        <select name="selectedFormatter">
+          <option value="MLA">MLA</option>
+          <option value="BibTeX">BibTex</option>
+          <option value="yaml">yaml</option>
+        </select>
+      </label>
   );
 }
 
-const IndexPage = () => {
-  const age: number = 0;
-
-  return (BasicSelect());
-};
-
-export default IndexPage;
+export default function Home() {
+  return (
+    <>
+      <header>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.min.css" />
+      </header>
+      <main>
+        <div>
+          {formatterSelect()}
+        </div>
+      </main>
+    </>
+  )
+}
